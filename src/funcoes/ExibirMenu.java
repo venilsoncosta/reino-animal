d39b873e8921964182118058cb5f4abd15337c53
venilson <venilsoncosta21@gmail.com>
2024-01-04 15:38:47 -0300

    deixando o projeto melhor

---
+++ src/funcoes/ExibirMenu.java
@@ -0,0 +1,12 @@
+package funcoes;
+
+public class ExibirMenu {
+    public static void exibirMenu() {
+        System.out.println("\nMenu:");
+        System.out.println("1 - Adicionar um animal");
+        System.out.println("2 - Remover um animal");
+        System.out.println("3 - Listar animais cadastrados");
+        System.out.println("4 - Sair");
+        System.out.print("Escolha uma opção: ");
+    }
+}
--- src/programa/Programa.java
+++ src/programa/Programa.java
@@ -1,32 +1,44 @@
 package programa;
 
-import classes.Zoologico;
-import mamiferos.Leao;
-import mamiferos.Zebra;
+
+import classes.Animal;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static funcoes.ExibirMenu.exibirMenu;
 
 public class Programa {
     public static void main(String[] args) {
-        Zebra zebra1 = new Zebra("zebrinha", "preta e branca", "Zebra");
-        //zebra1.comer();
-        System.out.println(zebra1.getPeso());
+        private static ArrayList<Animal> animais = new ArrayList<>();
+        private static Scanner scanner = new Scanner(System.in);
 
-        Leao zeus = new Leao("zeus", "laranja", "Leão");
-        //zeus.comer(zebra1);
-        System.out.println(zeus.getPeso());
-        System.out.println(zeus.getEspecie());
+            int opcao;
 
-        Zoologico zoo = new Zoologico();
-        zoo.adicionarAnimal(zeus);
-        zoo.adicionarAnimal(zebra1);
-        zoo.adicionarAnimal(zeus);
-        zoo.adicionarAnimal(zeus);
-        zoo.adicionarAnimal(zeus);
-        zoo.adicionarAnimal(zeus);
-        zoo.adicionarAnimal(zeus);
+            do {
+                exibirMenu();
+                opcao = scanner.nextInt();
 
-        zoo.exibirAnimais();
-        System.out.println(zoo.getQuantidadeAnimais());
+                switch (opcao) {
+                    case 1:
+                        adicionarCachorro();
+                        break;
+                    case 2:
+                        removerAnimal();
+                        break;
+                    case 3:
+                        listarAnimais();
+                        break;
+                    case 4:
+                        System.out.println("Saindo do programa. Até mais!");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Tente novamente.");
+                }
 
+            } while (opcao != 4);
 
+            scanner.close();
     }
 }
+
